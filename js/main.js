21cd31c0b2458004de6af08214c9127ceec29e13
Haegnim <haegnim@gmail.com>
2021-08-06 23:07:56 +0900

    0806 가로스크롤 수정 & 컨텐츠 추가

--- js/main.js
+++ js/main.js
@@ -7,9 +7,10 @@ function horawheel() {
   // let scrollX = 1;
   // const workVeiwX = workVeiw.offsetWidth;
   const workX = work[1].offsetWidth;
+  const HomeX = Home.offsetWidth;
   // console.log(workVeiwX);
 
-  const workListX = work.length * (workX + 70) - 70;
+  const workListX = work.length * (workX + 70) + 70;
   workList.style.width = workListX + "px";
 
   let tranX = 1;
@@ -18,20 +19,34 @@ function horawheel() {
       console.log("wheel up");
       tranX += 150;
     } else {
-      Home.style.transform = "translateX(-35%)";
+      Info.style.opacity = 0;
+      workVeiw.style.background = "#fff";
       console.log("wheel down");
       tranX -= 150;
     }
 
     if (tranX > 0) {
       tranX = 0;
-      Home.style.transform = "translateX(0%)";
-      // Home.style.transform = "translateX(0%)";
-    } else if (Math.abs(tranX) > workListX / 3) {
-      tranX = -(workListX / 3);
+      Info.style.opacity = 1;
+      workVeiw.style.background = "#efefef";
+    } else if (workListX < Math.abs(tranX)) {
+      tranX = -1 * workListX;
     }
-    workList.style.transform = "translateX(" + tranX + "px)";
+    Home.scrollLeft = -1 * tranX;
+    console.log(Home.scrollLeft);
+    console.log(HomeX);
     console.log(tranX);
   };
 }
 horawheel();
+
+function hambergerClick() {
+  const hamberger = document.querySelector(".hamberger");
+  const tabnav = document.querySelector("aside");
+
+  hamberger.addEventListener("click", function () {
+    tabnav.classList.toggle("on");
+    hamberger.classList.toggle("on");
+  });
+}
+hambergerClick();
