ab03414a5b467fa563865a9c33822d9cda73bb07
Haegnim <84562770+Haegnim@users.noreply.github.com>
2021-08-16 17:08:28 +0900

    0815
    깃 데스트탑 테스트
    가로 스크롤 이벤트 수정
    쿠키 이벤트 생성
    카드 요소 추가
    인포 타이포그래피 수정

--- js/main.js
+++ js/main.js
@@ -1,42 +1,50 @@
 function horawheel() {
-  const Home = document.querySelector(".home");
-  const workVeiw = document.querySelector(".work-veiw");
   const workList = document.querySelector(".work-list");
   const work = document.querySelectorAll(".work-list li");
+  const workVeiw = document.querySelector(".work-veiw");
+  const Home = document.querySelector(".home");
   const Info = document.querySelector(".info");
   // let scrollX = 1;
   // const workVeiwX = workVeiw.offsetWidth;
-  const workX = work[1].offsetWidth;
+  const workX = work[0].offsetWidth;
   const HomeX = Home.offsetWidth;
+  const InfoX = Info.offsetWidth;
   // console.log(workVeiwX);
 
   const workListX = work.length * (workX + 70) + 70;
   workList.style.width = workListX + "px";
 
-  let tranX = 1;
-  window.onmousewheel = function (e) {
-    if (e.wheelDelta >= 120) {
-      console.log("wheel up");
-      tranX += 150;
+  let offset = 0;
+
+  window.addEventListener("wheel", e => {
+    // console.log(e);
+    if (Math.abs(e.deltaY) < 120) {
+      // console.log("delay");
+      e.deltaX = 0;
+      offset += Math.sign(e.deltaY) * 25;
     } else {
+      offset += Math.sign(e.deltaY) * 150;
+      console.log("delay");
+    }
+    console.log(offset);
+    if (offset >= 120) {
       Info.style.opacity = 0;
       Home.style.background = "#fff";
-      console.log("wheel down");
-      tranX -= 150;
-    }
-
-    if (tranX > 0) {
-      tranX = 0;
+      workVeiw.style.background = "#fff";
+      // console.log("wheel down");
+    } else {
       Info.style.opacity = 1;
       Home.style.background = "#efefef";
-    } else if (workListX < Math.abs(tranX)) {
-      tranX = -1 * workListX;
+      workVeiw.style.background = "#efefef";
     }
-    Home.scrollLeft = -1 * tranX;
-    console.log(Home.scrollLeft);
-    console.log(HomeX);
-    console.log(tranX);
-  };
+
+    if (offset < 0) {
+      offset = 0;
+    } else if (offset > workListX - workX) {
+      offset = workListX - workX;
+    }
+    workList.style.transform = `translateX(-${offset}px`;
+  });
 }
 horawheel();
 
@@ -50,3 +58,63 @@ function hambergerClick() {
   });
 }
 hambergerClick();
+
+function introOut() {
+  const intro = document.querySelector(".intro");
+  // console.log(intro);
+  // intro.style.display = "none";
+  setTimeout(function () {
+    intro.style.display = "none";
+  }, 4000);
+}
+introOut();
+
+function setCookie(cname, cvalue, min) {
+  const exdate = new Date();
+  exdate.setMinutes(exdate.getMinutes() + min);
+  // d.setTime(d.getTime() + days * 24 * 60 * 60 * 1000);
+  const expires = "expires=" + exdate.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+// setCookie("open", "started", "exdays");
+
+function getCookie(cname) {
+  const name = cname + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  console.log("get");
+  return "";
+}
+getCookie(open);
+
+function checkCookie() {
+  const intro = document.querySelector(".intro");
+  const workVeiw = document.querySelector(".work-veiw");
+  const Home = document.querySelector(".home");
+  const Info = document.querySelector(".info");
+  const header = document.querySelector(".header");
+
+  const checkIntro = getCookie("open");
+  if (checkIntro != "") {
+    // alert("잘 오셨어요. " + intro + " 님");
+    intro.classList.toggle(checkIntro);
+  } else {
+    setCookie("open", "started", 3);
+    workVeiw.classList.toggle("start");
+    Home.classList.toggle("start");
+    Info.classList.toggle("start");
+    header.classList.toggle("start");
+  }
+}
+checkCookie();
